3afc44e69ed86f7a7ad52bf85f9c292c9431993d
SongNoin <as5427072@gmail.com>
2023-02-07 23:36:57 +0900

    FEAT react-router, 샘플페이지 세팅

---
+++ src/commons/routes/AppRouter.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "../../pages/Home";
+import SignInPage from "../../pages/SignIn";
+import SignUpPage from "../../pages/SignUp";
+
+export default function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+      </Routes>
+    </Router>
+  );
+}
